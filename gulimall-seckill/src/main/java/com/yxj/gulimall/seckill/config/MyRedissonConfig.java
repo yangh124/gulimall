d2ba53074a228baca197c25676f5aa50758039d2
yanghao <yh.124@qq.com>
2022-06-12 13:57:36 +0800

    first commit

---
+++ gulimall-seckill/src/main/java/com/yxj/gulimall/seckill/config/MyRedissonConfig.java
@@ -0,0 +1,30 @@
+package com.yxj.gulimall.seckill.config;
+
+import org.redisson.Redisson;
+import org.redisson.api.RedissonClient;
+import org.redisson.config.Config;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.io.IOException;
+
+/**
+ * @author yaoxinjia
+ */
+@Configuration
+public class MyRedissonConfig {
+
+    @Value("${spring.redis.host}")
+    private String host;
+    @Value("${spring.redis.port}")
+    private int port;
+
+    @Bean(destroyMethod = "shutdown")
+    RedissonClient redisson() throws IOException {
+        Config config = new Config();
+        config.useSingleServer().setAddress("redis://" + host + ":" + port);
+        return Redisson.create(config);
+    }
+
+}
